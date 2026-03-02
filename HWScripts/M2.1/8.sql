8e9a1c5ebcb0202997449c212db5b7eeab12205f
saul <saulifshin.cs@gmail.com>
2026-03-01 16:55:04 -0800

    finished m2.1

--- HWScripts/M2.1/8.sql
+++ HWScripts/M2.1/8.sql
@@ -12,8 +12,18 @@ FROM vendors
 WHERE vendor_contact_last_name LIKE 'a%' OR vendor_contact_last_name LIKE 'b%' OR vendor_contact_last_name LIKE 'c%' OR vendor_contact_last_name LIKE 'e%'
 ORDER BY vendor_contact_last_name ASC;
 
--- 10
 
+-- 10
+SELECT invoice_due_date         AS "Due Date",
+       invoice_total            AS "Invoice Total",
+       invoice_total * 0.10     AS "10%",
+       invoice_total * 1.10     AS "Plus 10%"
+FROM   Invoices
+WHERE invoice_total >= 500
+  AND invoice_total <= 1000
+ORDER BY invoice_due_date DESC;
+
+-- 11
 
 SELECT invoice_number, invoice_total, payment_total + credit_total AS payment_credit_total, invoice_total - payment_total - credit_total AS balance_due
 FROM invoices
@@ -22,10 +32,24 @@ ORDER BY invoice_total - payment_total - credit_total DESC
 LIMIT 5;
 
 
--- 11
+-- 12
 
 SELECT invoice_number,invoice_date,invoice_total - payment_total - credit_total,payment_date
 FROM invoices 
-WHERE payment_date is NULL
+WHERE payment_date is NULL;
+
+-- 13
+SELECT 
+	-- Returns today's date in default YYYY-MM-DD format
+    CURRENT_DATE,
+    -- Reformats the date to mm-dd-yyyy
+    DATE_FORMAT(CURRENT_DATE, '%m-%d-%Y') AS 'current_date';
+    
+-- 14
+SELECT
+  50000                    AS starting_principal,
+  50000 * 0.065            AS interest,
+  50000 + 50000 * 0.065    AS principal_plus_interest;
+
 
 
